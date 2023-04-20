2a22d3b62ce7682ec51b9bbb74244cacbd26ac55
Charlie Wisoff <cwisoff@gmail.com>
2023-04-20 14:41:51 -0600

    fix: fix read_file    - make read_file more resistant to ai mistakes    - keep the most recent successful result of listful in the messages list

--- bot/app.py
+++ bot/app.py
@@ -13,14 +13,27 @@ class ChatBot:
     def __init__(self, system=""):
         self.system = system
         self.messages = []
+        self.last_list_files_result = None
         if self.system:
             self.messages.append({"role": "system", "content": system})
 
     def __call__(self, message):
-        print(self.messages)
+        # memory summarization: keep the action result if it was the last successful list_files action otherwise
+        # replace it with a message saying to check the next message to see what you thought about this message
         if len(self.messages) >= 2 and self.messages[-2]['role'] == 'user':
-            self.messages[-2]['content'] = 'check the next message to see what you thought about this message'
+            message_content = self.messages[-2]['content']
+            successful_list_files_re = re.compile('list_files.*files found')
+            if successful_list_files_re.search(message_content) and (not self.last_list_files_result or len(self.last_list_files_result) > len(message_content)):
+                self.last_list_files_result = message_content
+                self.messages[-2]['content'] = "last successful result of list_files: {}".format(message_content)
+                for old_message in self.messages[:-2]:
+                    list_files_re = re.compile('list_files')
+                    if list_files_re.search(old_message['content']) and old_message['role'] == 'user':
+                        old_message['content'] = 'check the next message to see what you thought about this message'
+            else:
+                self.messages[-2]['content'] = 'check the next message to see what you thought about this message'
         self.messages.append({"role": "user", "content": message})
+        # print(self.messages)
         result = self.execute()
         self.messages.append({"role": "assistant", "content": result})
         return result
@@ -50,25 +63,25 @@ Use the result of this action to figure out which file to look at or if you need
 
 read_file:
 e.g. task: figure out why my code isn't working, file: /main/app.py
-Use the initial coding task and any previous action results to call this action with a file path.
-Use the result of this action to figure out which line(s) of code to change or if you need first look in another file.
+Use the initial coding task and any previous list_files action results to call this action with a file path.
+Use the result of this action to figure out which line(s) of code to change or if you to need first look in another file.
 
 change_file:
 e.g. task: figure out why my code isn't working, file: /main/app.py
 Given a file path and a dictionary of file lines and corresponding proposed changes, update the file with your proposed changes
 
-Your goal is to iterate through these actions with the help of a developer until the original coding task is achieved
+Your goal is to iterate through these actions until the original coding task is achieved
 
 Example session:
 
 Coding Task: We have a built game of Tetris. The arrow keys are used to move pieces, but pressing up also scrolls the
              user up and down in the browser. Prevent this from happening.
 Thought: I should find the file where arrow keyboard events are handled.
-Action: list_files: /lib
+Action: list_files: /
 PAUSE
 
-Thought: Given the results of list_files: /lib, I should look in the /frontend/eventHandlers.js file.
-Action: read_file: /frontend/eventHandlers.js 
+Thought: Given the results of list_files: /, I should look in the /frontend/eventHandlers.js file.
+Action: read_file: /frontend/eventHandlers.js
 PAUSE
 
 Continue iterating like this until you make the necessary changes to complete the coding task.
@@ -78,7 +91,7 @@ Continue iterating like this until you make the necessary changes to complete th
 action_re = re.compile('^Action: (\w+): (.*)$')
 
 
-def coding_task(task, max_turns=5):
+def coding_task(task, max_turns=8):
     i = 0
     bot = ChatBot(prompt)
     next_prompt = task
@@ -124,10 +137,9 @@ def list_files(starting_dir=''):
 def read_file(filepath):
     file_lines = []
     num = 1
-    # use len to keep track of each line and then keep a total count of char/tokens 4:1 ratio
-    # then stop if, too much
+    truncated_file_path = filepath if filepath[0] == '/' else filepath[1:]
     try:
-        with open(".{}".format(filepath[1:])) as file:
+        with open(".{}".format(truncated_file_path)) as file:
             for line in file:
                 file_lines.append("{}. {}".format(num, line))
                 num += 1
